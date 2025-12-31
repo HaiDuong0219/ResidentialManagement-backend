14384c22cad1807cebf46df6a145c37f3b9c6259
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2025-12-31 08:14:54 +0700

    Feat: Resident Log

--- src/routes/householdsRoutes.js
+++ src/routes/householdsRoutes.js
@@ -10,6 +10,8 @@ import {
     deleteHousehold
 } from '../controllers/householdsController.js';
 
+import { getHouseholdResidentLogs } from '../controllers/residentLogsController.js';
+
 const router = express.Router();
 
 router.post('/', createHousehold);
@@ -17,6 +19,7 @@ router.get('/', getAllHouseholds);
 router.get('/code/:household_code', getHouseholdByCode);
 router.get('/:household_code/residents', getHouseholdResidents);
 router.post('/:id/split', splitHousehold);
+router.get('/:id/resident-logs', getHouseholdResidentLogs);
 router.get('/:id', getHouseholdById);
 router.put('/:id', updateHousehold);
 router.delete('/:id', deleteHousehold);
